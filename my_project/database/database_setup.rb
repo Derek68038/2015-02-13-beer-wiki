e7facfd90ddef70e4ebfddb6429582bcea600d1b
Derek George <derek68038@gmail.com>
2015-02-26 09:00:34 -0600

    In process of restructuring folders.

--- my_project/database/database_setup.rb
+++
@@ -1,9 +0,0 @@
-DATABASE.results_as_hash = true
-
-DATABASE.execute("CREATE TABLE IF NOT EXISTS styles (id INTEGER PRIMARY KEY, style TEXT, color TEXT, 
-                  bitterness TEXT, alcohol TEXT, flavor TEXT, related TEXT)")
-
-DATABASE.execute("CREATE TABLE IF NOT EXISTS beers (id INTEGER PRIMARY KEY, beer TEXT, style_id INTEGER, 
-                  color TEXT, ibu INTEGER, abv REAL, brewery_id INTEGER, review TEXT, date TEXT)")
-
-DATABASE.execute("CREATE TABLE IF NOT EXISTS breweries (id INTEGER PRIMARY KEY, brewery TEXT, description TEXT)")
