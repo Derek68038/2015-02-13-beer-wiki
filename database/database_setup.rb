aa80dc3a9f242177e94a35af75dd114e8c5f91cf
Derek George <derek68038@gmail.com>
2015-03-13 11:27:08 -0500

    Setup AR DB connection rather than using SQLite3.

--- database/database_setup.rb
+++ database/database_setup.rb
@@ -1,15 +1,45 @@
 DATABASE.results_as_hash = true
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS styles (id INTEGER PRIMARY KEY, category TEXT, color TEXT, 
-                  bitterness TEXT, alcohol TEXT, flavor TEXT, related TEXT)")
+unless ActiveRecord::Base.connection.table_exists?(:styles)
+  ActiveRecord::Base.connection.create_table :styles do |t|
+    t.text :category
+    t.text :color
+    t.text :bitterness
+    t.text :alcohol
+    t.text :flavor
+    t.text :related
+  end  
+end
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS beers (id INTEGER PRIMARY KEY, name TEXT, style_id 
-                  INTEGER, color TEXT, ibu INTEGER, abv REAL, review TEXT, date 
-                  TEXT)")
+unless ActiveRecord::Base.connection.table_exists?(:beers)
+  ActiveRecord::Base.connection.create_table :beers do |t|
+    t.text :name
+    t.integer :style_id
+    t.text :color
+    t.integer :ibu
+    t.float :abv
+    t.text :review
+    t.text :date
+  end  
+end
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS breweries (id INTEGER PRIMARY KEY, place TEXT, 
-                  description TEXT)")
+unless ActiveRecord::Base.connection.table_exists?(:breweries)
+  ActiveRecord::Base.connection.create_table :breweries do |t|
+    t.text :place
+    t.text :description
+  end  
+end
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS beers_breweries (beer_id INTEGER, brewery_id INTEGER)")
+unless ActiveRecord::Base.connection.table_exists?(:beers_breweries)
+  ActiveRecord::Base.connection.create_table :beers_breweries do |t|
+    t.integer :beer_id
+    t.integer :brewery_id
+  end  
+end
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, user TEXT, password TEXT)") 
+unless ActiveRecord::Base.connection.table_exists?(:users)
+  ActiveRecord::Base.connection.create_table :users do |t|
+    t.text :user
+    t.text :password
+  end  
+end
