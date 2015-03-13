a1d251a6ebe47f78b54b34f5586375b865c8d318
Derek George <derek68038@gmail.com>
2015-03-13 13:41:34 -0500

    Added new route for adding a new brewery and updated the new brewery erb file.

--- controllers/add_routes.rb
+++ controllers/add_routes.rb
@@ -9,18 +9,27 @@ post "/add_beer_results" do
 end
 
 get "/add_style" do
+  erb :"add/add_style"
+end
+
+post "/add_style_results" do
   @style = Style.new(params)
   @style.save
   redirect "/beer_styles"
 end
 
-get "/add_brewery" do
-  @brewery = Brewery
-  # erb :"add/add_brewery"
+get "add_brewery" do
+  erb :"add/add_brewery"
+end
+  
+post "/add_brewery_results" do
+  @brewery = Brewery.new(params)
+  @brewery.save
+  redirect "/beer_breweries"
 end
 
-get "/add_brewery_results" do
-  beer_mapper
-  @brewery = Brewery.new(params)
-  erb :"add/add_brewery_results"
-end
+# get "/add_brewery_results" do
+#   beer_mapper
+#   @brewery = Brewery.new(params)
+#   erb :"add/add_brewery_results"
+# end
