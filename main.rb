9104050f514ab1cc9d9a279ad39d099a1c69e8c9
Derek George <derek68038@gmail.com>
2015-03-13 11:21:04 -0500

    Created a Production DB.

--- main.rb
+++ main.rb
@@ -13,6 +13,18 @@ configure :development do
  set :database, {adapter: "sqlite3", database: "./database/beer_wiki.db"}
 end
 
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'])
+ ActiveRecord::Base.establish_connection(
+ :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+ :host => db.host,
+ :username => db.user,
+ :password => db.password,
+ :database => db.path[1..-1],
+ :encoding => 'utf8'
+ )
+end
+
 require_relative "database/database_methods"
 require_relative "helpers/helper"
 require_relative "models/beer"
